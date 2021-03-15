5fd63ac9360e0a7d4dbc51d978a299a328ebbe08
Luiz Henrique <lmenezesmessias@gmail.com>
2021-03-15 17:28:30 -0300

    Finalização

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace IMC
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+        {        
+            Console.WriteLine("Para saber seu imc, digite:");
+
+            Console.WriteLine("Sua altura (M): ");
+            double altura = Convert.ToDouble(Console.ReadLine()); 
+
+            Console.WriteLine("Seu peso (KG): ");
+            double peso = Convert.ToDouble(Console.ReadLine()); 
+
+            double IMC = Math.Pow(altura, 2); 
+            double resultado = peso/IMC; 
+
+            Console.WriteLine($"Seu IMC é: {resultado:N1} kg/m²");
+
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # IMC
-Calcule o índice de massa corporal de uma pessoa, dados altura (em m) e peso (em kg).  IMC = peso / altura²
+
+Calcule o índice de massa corporal de uma pessoa, dados altura (em m) e peso (em kg).  
+
+IMC = peso / altura²
+
+```cs
+Altura (m)..: 2
+Peso (kg)...: 80
+
+IMC: 20,0 kg/m²
+```
